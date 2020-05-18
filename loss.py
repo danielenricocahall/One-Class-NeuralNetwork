10a551512109413a04dd6ce8a2730886a8830d20
danielenricocahall <danielenricocahall@gmail.com>
2020-05-17 21:42:24 -0400

    fix arg name

--- loss.py
+++ loss.py
@@ -1,13 +1,13 @@
 import keras.backend as K
 
 
-def quantile_loss(r, y_n, nu):
+def quantile_loss(r, y, nu):
     """
     3rd term in Eq (3) of the original paper
     :param r: bias of hyperplane
-    :param y_n: data / output we're operating on
+    :param y: data / output we're operating on
     :param nu: parameter between [0, 1] controls trade off between maximizing the distance of the hyperplane from
         the origin and the number of data points permitted to cross the hyper-plane (false positives) (default 1e-2)
     :return: the loss function value
     """
-    return (1 / nu) * K.mean(K.maximum(0.0, r - y_n)) - r
+    return (1 / nu) * K.mean(K.maximum(0.0, r - y)) - r
--- test/test_loss_function.py
+++ test/test_loss_function.py
@@ -16,8 +16,8 @@ def test_loss_function(tf_session):
     # GIVEN we have this data (integers from 1-9) and nu = 1/3 (0.33)
     data = [1, 2, 3, 4, 5, 6, 7, 8, 9]
     nu = 0.33
-    y_n = tf.convert_to_tensor([data], dtype=tf.float32)
+    y = tf.convert_to_tensor([data], dtype=tf.float32)
     # WHEN we execute the quantile loss function on each value in the data
-    results = {r: tf_session.run(quantile_loss(r, y_n, nu)) for r in data}
+    results = {r: tf_session.run(quantile_loss(r, y, nu)) for r in data}
     # THEN the argument which gives us the minimum value should be 3
     assert next(k for k, v in results.items() if v == min(results.values())) == 3
