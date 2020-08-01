6e4c0155506bfb7a2c1f16b8f9b6fc6c609e23e8
danielenricocahall <danielenricocahall@gmail.com>
2020-08-01 17:11:20 -0400

    update tests

--- test/test_basic.py
+++ test/test_basic.py
@@ -5,21 +5,14 @@ from loss import quantile_loss
 from ocnn import OneClassNeuralNetwork
 
 
-@fixture
-def tf_session():
-    session = tf.Session()
-    yield session
-    session.close()
-
-
-def test_loss_function(tf_session):
+def test_loss_function():
     # Test case described in the paper
     # GIVEN we have this data (integers from 1-9) and nu = 1/3 (0.33)
     data = [1, 2, 3, 4, 5, 6, 7, 8, 9]
     nu = 0.33
     y = tf.convert_to_tensor([data], dtype=tf.float32)
     # WHEN we execute the quantile loss function on each value in the data
-    results = {r: tf_session.run(quantile_loss(r, y, nu) - r) for r in data}
+    results = {r: quantile_loss(r, y, nu) - r for r in data}
     # THEN the argument which gives us the minimum value should be 3
     assert next(k for k, v in results.items() if v == min(results.values())) == 3
 
