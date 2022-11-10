84f5455f6f6be4acbe93e7664dced1295f19ef5f
danielenricocahall <danielenricocahall@gmail.com>
2022-11-10 14:11:08 -0500

    correct loss function

--- test/test_basic.py
+++ test/test_basic.py
@@ -1,5 +1,5 @@
 import tensorflow as tf
-from pytest import fixture
+from flatbuffers.builder import np
 
 from loss import quantile_loss
 from ocnn import OneClassNeuralNetwork
@@ -12,9 +12,11 @@ def test_loss_function():
     nu = 0.33
     y = tf.convert_to_tensor([data], dtype=tf.float32)
     # WHEN we execute the quantile loss function on each value in the data
-    results = {r: quantile_loss(r, y, nu) - r for r in data}
+    results = {r: quantile_loss(r, y, nu) for r in data}
     # THEN the argument which gives us the minimum value should be 3
-    assert next(k for k, v in results.items() if v == min(results.values())) == 3
+    minimum_argument, minimum_value = next((k, v) for k, v in results.items() if v == min(results.values()))
+    assert minimum_argument == 3
+    assert np.isclose(minimum_value, -1.99, atol=0.01)
 
 
 def test_build_model():
